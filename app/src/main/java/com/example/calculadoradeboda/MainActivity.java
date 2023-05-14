52d2cef8417452838061648ea57713b7d54a9277
SrFastGestor <hayamaquerica@gmail.com>
2023-05-14 18:44:10 +0200

    update titulo y logo

--- app/src/main/java/com/example/calculadoradeboda/MainActivity.java
+++ app/src/main/java/com/example/calculadoradeboda/MainActivity.java
@@ -35,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void  presion(View v){
-        Toast.makeText(this,"Calculando",Toast.LENGTH_LONG).show();
+        Toast.makeText(this,"Pensando, porque el que piensa es Sabio y nadie nacio sabiendo :)",Toast.LENGTH_LONG).show();
         String v1=edad.getText().toString();
         String v2=altura.getText().toString();
 
@@ -52,15 +52,15 @@ public class MainActivity extends AppCompatActivity {
         String fraseEstacion = "";
 
         if (rv.isChecked()){
-            fraseEstacion = "al ser en verano tendras que comprar un par de camellos tambien";
+            fraseEstacion = "Al ser en verano tendras que comprar un par de camellos tambien";
         } else if (ri.isChecked()) {
-            fraseEstacion = "al ser en invierno tendras que comprar un par de alces tambien";
+            fraseEstacion = "Al ser en invierno tendras que comprar un par de alces tambien";
         } else if (ro.isChecked()) {
-            fraseEstacion = "al ser en otoño tendras que comprar ... quien cojones se casa en otoño tu eres tonto, pues te toca comprar un dragon";
+            fraseEstacion = "Al ser en otoño tendras que comprar ... quien cojones se casa en otoño tu eres tonto, pues te toca comprar un dragon";
         } else if (rp.isChecked()) {
-            fraseEstacion = "al ser en primavera tendras que comprar un par de vacas";
+            fraseEstacion = "Al ser en primavera tendras que comprar un par de vacas";
         }else {
-            fraseEstacion = "si te da igual la estacion esque tiene billetes, Pagame por la app perro!!!";
+            fraseEstacion = "Si te da igual la estacion esque tiene billetes, Pagame por la app perro!!!";
         }
 
         tv1.setText("Tendras que pagar: " + totalCabras + " cabras y " + totalPollossinDecimal + " pollos para poder pedirle la mano a tus suegros . \n" + fraseEstacion );
